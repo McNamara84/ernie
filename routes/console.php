c3f59c7e16a8f8d3cad128addaea0338fb48797e
Holger Ehrmann <holgerehrmann@gmail.com>
2025-09-04 15:08:18 +0200

    feat: add add-user artisan command

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,19 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('add-user {name} {email} {password}', function (string $name, string $email, string $password) {
+    $user = User::create([
+        'name' => $name,
+        'email' => $email,
+        'password' => Hash::make($password),
+    ]);
+
+    $this->info("User {$user->email} created.");
+})->purpose('Add a new user to the database');
