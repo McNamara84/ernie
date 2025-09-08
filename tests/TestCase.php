8d8dc580fcfc24e431c307968fea169a987b0fb2
Holger Ehrmann <holgerehrmann@gmail.com>
2025-09-08 09:41:11 +0200

    test: stub vite manifest for tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,45 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\File;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $manifest = [
+            'resources/js/app.tsx' => [
+                'file' => 'resources/js/app.tsx',
+                'src' => 'resources/js/app.tsx',
+                'isEntry' => true,
+            ],
+        ];
+
+        foreach (File::allFiles(resource_path('js/pages')) as $file) {
+            if ($file->getExtension() !== 'tsx') {
+                continue;
+            }
+
+            if (str_contains($file->getPathname(), '__tests__')) {
+                continue;
+            }
+
+            $path = 'resources/js/pages/'.$file->getRelativePathname();
+
+            $manifest[$path] = [
+                'file' => $path,
+                'src' => $path,
+                'isEntry' => true,
+            ];
+        }
+
+        File::ensureDirectoryExists(public_path('build'));
+
+        File::put(
+            public_path('build/manifest.json'),
+            json_encode($manifest)
+        );
+    }
 }
