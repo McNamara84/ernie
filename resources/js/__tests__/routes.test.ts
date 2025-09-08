474d8c32dff23e50f778ffb2e21b7b280d7107d7
Holger Ehrmann <holgerehrmann@gmail.com>
2025-09-08 06:52:46 +0200

    test: handle method override queries

--- resources/js/__tests__/routes.test.ts
+++ resources/js/__tests__/routes.test.ts
@@ -7,7 +7,16 @@ describe('generated routes', () => {
     expect(home.url()).toBe('/');
     expect(home.url({ query: { page: 1 } })).toBe('/?page=1');
     expect(home.form.get()).toEqual({ action: '/', method: 'get' });
-    expect(home.form.head({ query: { foo: 'bar' } })).toEqual({ action: '/?_method=HEAD&foo=bar', method: 'get' });
+    expect(home.form.head({ query: { foo: 'bar' } })).toEqual({
+      action: '/?_method=HEAD&foo=bar',
+      method: 'get',
+    });
+    window.history.replaceState({}, '', '/?page=1');
+    expect(home.form.head({ mergeQuery: { foo: 'bar' } })).toEqual({
+      action: '/?page=1&_method=HEAD&foo=bar',
+      method: 'get',
+    });
+    window.history.replaceState({}, '', '/');
   });
 
   it('generates logout route definitions', () => {
