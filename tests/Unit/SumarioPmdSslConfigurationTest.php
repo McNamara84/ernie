ad29e1de625df9c310b5ef4fd02b47b86e214386
Holger Ehrmann <holgerehrmann@gmail.com>
2025-10-02 03:34:58 +0200

    Refine SUMARIOPMD SSL config tests

--- tests/Unit/SumarioPmdSslConfigurationTest.php
+++ tests/Unit/SumarioPmdSslConfigurationTest.php
@@ -1,17 +1,17 @@
 <?php
 
-function rememberSumarioPmdSslCaEnv(): array
-{
+beforeEach(function () {
     $value = getenv('DB_SUMARIOPMD_SSL_CA');
 
-    return [
+    $this->previousSumarioPmdSslCa = [
         'value' => $value === false ? null : $value,
         'had' => $value !== false,
     ];
-}
+});
 
-function restoreSumarioPmdSslCaEnv(array $state): void
-{
+afterEach(function () {
+    $state = $this->previousSumarioPmdSslCa;
+
     if ($state['had']) {
         putenv('DB_SUMARIOPMD_SSL_CA=' . $state['value']);
         $_ENV['DB_SUMARIOPMD_SSL_CA'] = $state['value'];
@@ -20,37 +20,26 @@ function restoreSumarioPmdSslCaEnv(array $state): void
         putenv('DB_SUMARIOPMD_SSL_CA');
         unset($_ENV['DB_SUMARIOPMD_SSL_CA'], $_SERVER['DB_SUMARIOPMD_SSL_CA']);
     }
-}
+});
 
 it('omits SUMARIOPMD SSL options when no certificate is configured', function () {
-    $state = rememberSumarioPmdSslCaEnv();
-
     putenv('DB_SUMARIOPMD_SSL_CA');
     unset($_ENV['DB_SUMARIOPMD_SSL_CA'], $_SERVER['DB_SUMARIOPMD_SSL_CA']);
 
-    try {
-        $config = require base_path('config/database.php');
+    $config = require base_path('config/database.php');
 
-        expect($config['connections']['metaworks']['options'])->toBe([]);
-    } finally {
-        restoreSumarioPmdSslCaEnv($state);
-    }
+    expect($config['connections']['metaworks']['options'])->toBe([]);
 });
 
 it('adds the configured SUMARIOPMD certificate to the connection options', function () {
-    $state = rememberSumarioPmdSslCaEnv();
     $certificatePath = base_path('docker/certs/sumariopmd-ca.pem');
 
     putenv('DB_SUMARIOPMD_SSL_CA=' . $certificatePath);
     $_ENV['DB_SUMARIOPMD_SSL_CA'] = $certificatePath;
     $_SERVER['DB_SUMARIOPMD_SSL_CA'] = $certificatePath;
 
-    try {
-        $config = require base_path('config/database.php');
+    $config = require base_path('config/database.php');
 
-        expect($config['connections']['metaworks']['options'][PDO::MYSQL_ATTR_SSL_CA] ?? null)
-            ->toBe($certificatePath);
-    } finally {
-        restoreSumarioPmdSslCaEnv($state);
-    }
+    expect($config['connections']['metaworks']['options'][PDO::MYSQL_ATTR_SSL_CA] ?? null)
+        ->toBe($certificatePath);
 });
