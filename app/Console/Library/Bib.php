644a1828cd44865e52d65ea207a99e9528953c7b
LeoWe09 <leo.weiss@fh-potsdam.de>
2026-06-17 10:40:21 +0200

    Delete Bib.php

--- app/Console/Library/Bib.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-
-use Nitotm\Eld\LanguageDetector;
-
-$eld = new LanguageDetector();
-
-$ergebnis = $eld->detect('Das ist ein Test.');
-echo $ergebnis->language;
