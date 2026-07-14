0d7445a3577cfbccdc4f72d202520da521ba18a4
emelylottabehrendt <emely-lotta.behrendt@fh-potsdam.de>
2026-07-14 21:46:52 +0200

    Replace Arrow icons with Alert and User icons

--- resources/js/pages/old-datasets.tsx
+++ resources/js/pages/old-datasets.tsx
@@ -1,6 +1,7 @@
 import { Head } from '@inertiajs/react';
 import axios, { isAxiosError } from 'axios';
-import { ArrowDown, ArrowUp, ArrowUpDown, ArrowUpRight, Trash2 } from 'lucide-react';
+import { AlertTriangle, Building2, Check, Plus, RefreshCw, User, X } from 'lucide-react';;
+
 import type { ReactNode } from 'react';
 import { useCallback, useEffect, useRef, useState } from 'react';
 import { toast } from 'sonner';
